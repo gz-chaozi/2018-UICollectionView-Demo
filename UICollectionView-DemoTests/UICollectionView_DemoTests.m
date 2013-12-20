cef7a1c0aca5f6eaee9fda60b3b710a402bd95d5
sany620 <371255920@qq.com>
2013-12-20 13:45:11 +0800

    Initial Commit

---
+++ UICollectionView-DemoTests/UICollectionView_DemoTests.m
@@ -0,0 +1,34 @@
+//
+//  UICollectionView_DemoTests.m
+//  UICollectionView-DemoTests
+//
+//  Created by leadingwinner on 13-12-20.
+//  Copyright (c) 2013年 leadingwinner. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface UICollectionView_DemoTests : XCTestCase
+
+@end
+
+@implementation UICollectionView_DemoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
