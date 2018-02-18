40e1af95912d8d7f1c3bb6375d25a7558668bd3f
chaozi <7986080@qq.com>
2018-02-18 23:29:05 +0800

    20180218第二次上传

--- UICollectionView-Demo/AppDelegate.m
+++ UICollectionView-Demo/AppDelegate.m
@@ -7,7 +7,7 @@
 //
 
 #import "AppDelegate.h"
-
+#import "ViewController.h"
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -15,6 +15,8 @@
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
+    self.viewController = [[ViewController alloc] initWithNibName:@"ViewController" bundle:nil] ;
+    self.window.rootViewController = self.viewController;
     [self.window makeKeyAndVisible];
     return YES;
 }
